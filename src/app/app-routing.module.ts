4714c5af497c7b1533f1cb4e96d9133fa6d9f71d
mor3uk <oldegs2017@gmail.com>
2019-11-30 15:57:29 +0400

    new guard added, check if there is the entity before loading its route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CheckIdGuard } from './check-id-guard.service';
 import { WorkoutComponent } from './workout/workout.component';
 import { WorkoutInfoComponent } from './workout/workout-create/workout-info/workout-info.component';
 import { WorkoutActionComponent } from './workout/workout-train/workout-action/workout-action.component';
@@ -15,15 +16,15 @@ const routes: Routes = [
       {
         path: 'exercises', component: WorkoutInfoComponent, children: [
           { path: '', component: ExerciseFormComponent },
-          { path: ':id', component: ExerciseFormComponent },
+          { path: ':id', canActivate: [CheckIdGuard], component: ExerciseFormComponent },
         ]
       },
-      { path: 'train/:id', component: WorkoutActionComponent },
+      { path: 'train/:id', canActivate: [CheckIdGuard], component: WorkoutActionComponent },
     ]
   },
   { path: 'results', component: ResultsComponent, children: [
     { path: '', component: ResultsInfoComponent },
-    { path: ':id', component: ResultsInfoComponent },
+    { path: ':id', canActivate: [CheckIdGuard], component: ResultsInfoComponent },
   ] },
   { path: '**', redirectTo: 'workout/exercises' },
 ];
---
+++ src/app/check-id-guard.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { WorkoutService } from './shared/services/workout.service';
+import { ExerciseService } from './shared/services/exercise.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CheckIdGuard implements CanActivate {
+  constructor(
+    private workoutService: WorkoutService,
+    private exerciseService: ExerciseService,
+    private router: Router,
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    const id = route.params.id;
+
+    switch ((<Function>route.component).name) {
+      case 'ExerciseFormComponent':
+        const currentExercise = this.exerciseService.getCurrentExerciseById(id);
+        if (currentExercise) {
+          return true;
+        }
+        this.router.navigate(['workout', 'exercises']);
+        return false;
+      case 'ResultsInfoComponent':
+      case 'WorkoutActionComponent':
+        const workout = this.workoutService.getWorkout(id);
+        if (workout) {
+          return true;
+        }
+        this.router.navigate(['results']);
+        return false;
+    }
+
+    return true;
+  }
+}
